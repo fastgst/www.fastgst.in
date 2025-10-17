9a53d8892881d3004fc6634dffd99c5072beafae
Sangeeth Nandakumar <sangeethnandakumar@gmail.com>
2025-10-17 23:52:23 +0530

    fix - Tailwind css integrated and build boosted

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,njk,md,js}",
+    "./src/_includes/**/*.njk",
+    "./src/_layouts/**/*.njk"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: 'rgb(34 197 94)',
+          600: 'rgb(22 163 74)',
+          700: 'rgb(21 128 61)',
+          800: '#166534',
+          900: '#14532d',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
+
